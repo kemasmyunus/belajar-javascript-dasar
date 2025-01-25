6fd31cb337ae14dd5e688c4ce36a8d9e183e9f5b
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-25 20:11:38 +0800

    belajar-javascript-dasar-hari2-commit4

---
+++ 6-variabel.js
@@ -0,0 +1,17 @@
+// variabel
+var namaLengkap = "Kemas M. Yunus";
+var kelas = "XII";
+
+//variabel bisa dipakai berkali-kali
+document.writeln(namaLengkap);
+document.writeln("</br>");
+document.writeln(namaLengkap);
+document.writeln("</br>");
+document.writeln(namaLengkap);
+document.writeln("</br>");
+
+// variabel bisa diubah valuenya
+namaLengkap = "Yunus aja";
+
+document.writeln(namaLengkap);
+document.writeln("</br>");
