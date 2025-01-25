6a9983ba5d1ab52d0668580834a69cc8e3e5d0e6
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-25 20:05:04 +0800

    belajar-javascript-dasar-hari2-commit2

---
+++ 5-tipe-data-string.js
@@ -0,0 +1,12 @@
+document.writeln("Kemas M. Yunus");
+document.writeln("</br>");
+
+document.writeln("Nama : " + "Kemas " + "Muhammad " + "Yunus");
+document.writeln("</br>");
+
+document.writeln("<textarea cols='100' rows='10'>");
+document.write("Kemas \nMuhammad \nYunus\n");
+document.write("\\Home\\Yuna\n");
+document.write("\"Belajar Javascript Dasar\"\n");
+document.write('\'Sumber : ProgrammerZamanNow\'\n');
+document.writeln("</textarea>");
