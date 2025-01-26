18a484e08f44c7f9750818024db13efc14c580d2
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-26 19:22:59 +0800

    belajar-javascript-dasar-hari3-commit2

---
+++ 8-variabel-const.js
@@ -0,0 +1,5 @@
+const cobaConst = "ini adalah value dari variabel const";
+//cobaConst = "udah dibilang gak bisa"; // ERROR
+
+document.writeln(cobaConst);
+document.writeln("<br>//variabel const tidak bisa diubah");
