d10345ea0dd21f5c9f22cc7ad9804a48dd78f828
kemasmyunus <7a9tqplxjh@gmail.com>
2025-01-26 19:18:10 +0800

    belajar-javascript-dasar-hari3-commit1

---
+++ 7-variabel-let.js
@@ -0,0 +1,5 @@
+// variabel let itu intinya variabel yang bisa diubah
+let umurPegawai = 20
+document.writeln(`umur pegawai sebelum diubah adalah ${umurPegawai}<br>`)
+umurPegawai = 21
+document.writeln(`umur pegawai setelah diubah adalah ${umurPegawai}`)
